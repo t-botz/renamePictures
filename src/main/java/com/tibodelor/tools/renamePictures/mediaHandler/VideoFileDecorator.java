07e6287d961c0da4c384ed196020b3b65999bb12
Tibo <delor.thibault@gmail.com>
2017-10-20 13:56:00 +1100

    Fix NPE

--- src/main/java/com/tibodelor/tools/renamePictures/mediaHandler/VideoFileDecorator.java
+++ src/main/java/com/tibodelor/tools/renamePictures/mediaHandler/VideoFileDecorator.java
@@ -58,7 +58,7 @@ public class VideoFileDecorator implements MediaFileDecorator {
 
     @Override
     public Path getPath() {
-        return null;
+        return inputFile;
     }
 
     @Override
